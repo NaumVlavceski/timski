fb387f257b90c65e37391f223f033c9135a038c4
Damjan <serafimovskid77@gmail.com>
2025-12-11 21:50:53 +0100

    add new questionnaires

--- timski_proekt/views.py
+++ timski_proekt/views.py
@@ -5,7 +5,8 @@ import json
 from django.shortcuts import render
 
 def index(request):
-    with open("timski_proekt/Prasalnici/data.json", encoding="utf-8") as f:
+    with open("timski_proekt/Prasalnici/20meseci.json", encoding="utf-8") as f:
+
         data = json.load(f)
 
     return render(request, "index.html", {"quiz": data})
